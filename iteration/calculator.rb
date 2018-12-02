b4a8c89298a38ef5457e658e4fa3013acfb3f67b
GabrielRM <gabrielrmodesto5@gmail.com>
2018-12-02 21:26:07 -0200

    How iterations works in ruby, for and while

--- iteration/calculator.rb
+++ iteration/calculator.rb
@@ -4,7 +4,7 @@ loop do
 	puts "1 - +"
 	puts "2 - -"
 	puts "3 - *"
-	puts "4 - /" 
+	puts "4 - /"
 	puts "5 - get out"
 	option = gets.chomp.to_i
 
@@ -16,7 +16,7 @@ loop do
 		first_number = gets.chomp.to_i
 		print "Type the second number "
 		second_number = gets.chomp.to_i
-		
+
 		case option
 		when 1
 			puts "#{first_number} + #{second_number} = #{first_number+second_number}"
@@ -25,7 +25,7 @@ loop do
 		when 3
 			puts "#{first_number} * #{second_number} = #{first_number*second_number}"
 		when 4
-			puts "#{first_number} / #{second_number} = #{first_number/second_number}"		
+			puts "#{first_number} / #{second_number} = #{first_number/second_number}"
 		end
 	else
 		puts "It is not a valid option!"
