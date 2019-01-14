813b7ae2310dc33beadaf9f02db04948fa4d8b3f
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-14 19:48:07 -0200

    Verification of cpf is functional

--- methods-gems/misson2.rb
+++ methods-gems/misson2.rb
@@ -1,1 +1,15 @@
 #Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de cpf e em um método verifique se este número é válido. 
+require 'cpf_cnpj'
+
+def cpf_check(cpf_number)
+	if CPF.valid?(cpf_number)
+		puts "Is valid"
+	else
+		puts "Not valid"
+	end
+end
+
+puts "What's the cpf number? "
+cpf_number = gets.to_i
+
+cpf_check(cpf_number)
